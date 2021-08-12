2e2e00ed180d6f219223e4e444393bd06688da6c
baydir <kidoni@live.fr>
2021-08-12 16:10:19 +0200

    fonction de recherche fonctionelle

---
+++ src/API/API.js
@@ -0,0 +1,9 @@
+const API_KEY = "abc817763a2a7c4405e13c58d571e89d"
+
+const weatherSearch = async (city) => {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=abc817763a2a7c4405e13c58d571e89d`)
+    const data = await response.json()
+    return data
+}
+
+export default weatherSearch
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,31 @@
 // Component Page d'accueil
+import weatherSearch from '../API/API'
+import { useState, useEffect } from 'react'
 
 const Home = () => {
+    const [city, setcity] = useState("")
+    const [data, setData] = useState({})
+
+    const dataResult = async (e) => {
+        e.preventDefault()
+        const dataTemp = await weatherSearch(city)
+        setData(dataTemp)
+        console.log(dataTemp)
+    }
+
     return (
         <div>
             <h1>Homepage</h1>
+            <div>
+                <form onSubmit={dataResult}>
+                    <input onChange={e => setcity(e.target.value)} type="text"></input>
+                    <input type="submit" value="Search"></input>
+                </form>
+            </div>
+            <div>
+                <p>City : {data.name}</p>
+                <p>Temperature : {data.main ? data.main.temp : null}</p>
+            </div>
         </div>
     )
 }
